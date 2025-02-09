48b10ee24e5dd00cefaedd14aab3acb4b3807447
namstutz188 <namstutz188@gmail.com>
2025-02-09 16:26:28 -0600

    Remove unnused variables in pokeApi.js

--- src/api/pokeAPI.js
+++ src/api/pokeAPI.js
@@ -1,20 +1,20 @@
 export async function getPokemons() {
     const domain = "https://pokeapi.co/";
     const endpoint = "api/v2/pokemon/";
+    /*
     const limitCount = "?limit=1";
-
+    
     const urlCount = domain + endpoint + limitCount;
 
     const responseCount = await fetch(urlCount);
     const jsonCount = await responseCount.json();
-        
+    
     //const pokemonCount = jsonCount.count;
 
     //Use pokemon count in limit for next call
 
     //TEMP: Only pull back 151 while testing
 
-    /*
     const limitAll = `?limit=${pokemonCount}`;
     const urlAll = domain + endpoint + limitAll;
     */
