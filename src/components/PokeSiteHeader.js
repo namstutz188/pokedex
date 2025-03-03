15089f164dcfe621772f870b82286bdc614a6829
namstutz188 <namstutz188@gmail.com>
2025-03-02 19:59:20 -0600

    Edit SiteHeader for correct name. Add units to table and with formatting,

--- src/components/PokeSiteHeader.js
+++ src/components/PokeSiteHeader.js
@@ -24,7 +24,7 @@ export default function PokeSiteHeader() {
                     sx = {{height: '100%', width: 'auto'}}
                 />
                 <Paper sx = {paperStyle}>
-                    PokédexLite
+                    PokéLite
                 </Paper>
                 
             </Box>
--- src/data/parseData.js
+++ src/data/parseData.js
@@ -1,10 +1,14 @@
 //Receives a pointer to an array and an object containing data to be parsed and put into an array
 
 export default function parseData(dataArray,dataObj) {
-    //Height Property
-    dataArray.push({key: 'Height', value: dataObj.height});
-    //Weight
-    dataArray.push({key: 'Weight', value: dataObj.weight});
+    //Height Property - in decimeters. Convert into meters
+    const heightMeters = dataObj.height / 10;
+    const heightValue = heightMeters !== 1 ? heightMeters + ' meters' : heightMeters + ' meter';
+    dataArray.push({key: 'Height', value: heightValue});
+    //Weight - in hectograms. Convert to kilograms
+    const weightKilogram = dataObj.weight / 10;
+    const weightValue = weightKilogram != 1 ? weightKilogram + ' kilograms' : weightKilogram + ' kilogram';
+    dataArray.push({key: 'Weight', value: weightValue});
     //Type One
     const typeOneName = dataObj.types[0].type.name
     const typeOneFormat = typeOneName.charAt(0).toUpperCase() + typeOneName.slice(1)
